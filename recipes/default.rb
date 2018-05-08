9fc26b7e9c97cd6836b9fedfe92866d5ac83cb44
Jonathan Pedersen <jonoped@gmail.com>
2018-05-08 13:23:55 +1200

    adds a line of text to default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,15 @@
 
 # some blah blah
 # Some more blah blah
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello, world!</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+Hello...this is a git push test
